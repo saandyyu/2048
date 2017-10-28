ca5ff085a257973353af111fd351b41680fab57a
Phu Ly <phuly21@gmail.com>
2017-10-28 02:09:31 -0400

    creating class

---
+++ 2048/BoardClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+    class BoardClass
+    {
+        const int Size = 4;
+        int[][] Board = new int[Size][];
+
+    }
+}
